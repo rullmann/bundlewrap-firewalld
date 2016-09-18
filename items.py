248a66711ec782d37198a556b9835e19d27d6834
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-09-18 15:54:04 +0200

    add option to set a default zone for all interfaces

--- items.py
+++ items.py
@@ -18,6 +18,21 @@ svc_systemd = {
     },
 }
 
+default_zone = node.metadata.get('firewalld', {}).get('default_zone')
+if node.metadata.get('firewalld', {}).get('default_zone'):
+    for interface in node.metadata['interfaces']:
+        actions['firewalld_set_default_zone_{}'.format(interface)] = {
+            'command': "firewall-cmd --permanent --zone={} --add-interface={}".format(default_zone, interface),
+            'unless': "firewall-cmd --list-interfaces --zone={} | grep {}".format(default_zone, interface),
+            'cascade_skip': False,
+            'needs': [
+                "pkg_yum:firewalld",
+            ],
+            'triggers': [
+                "action:firewalld_reload",
+            ],
+        }
+
 for port in node.metadata.get('firewalld', {}).get('ports', {}):
     actions['firewalld_add_port_{}'.format(port)] = {
         'command': "firewall-cmd --permanent --add-port={}".format(port),
