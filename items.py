8f70a8b6c0587e72fb043ec24cd0c8e29c5f34a8
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-09-18 12:05:46 +0200

    add option to open custom ports

--- items.py
+++ items.py
@@ -17,3 +17,16 @@ svc_systemd = {
         ],
     },
 }
+
+for port in node.metadata.get('firewalld', {}).get('ports', {}):
+    actions['firewalld_add_port_{}'.format(port)] = {
+        'command': "firewall-cmd --permanent --add-port={}".format(port),
+        'unless': "firewall-cmd --list-ports | grep {}".format(port),
+        'cascade_skip': False,
+        'needs': [
+            "pkg_yum:firewalld",
+        ],
+        'triggers': [
+            "action:firewalld_reload",
+        ],
+    }
