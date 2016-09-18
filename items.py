077be9c270105dac36cf2c486d58d5e1c5d5f3a7
Rico Ullmann <rico@erinnerungsfragmente.de>
2016-09-18 17:13:57 +0200

    set additional ports according to default or custom zone options

--- items.py
+++ items.py
@@ -18,8 +18,8 @@ svc_systemd = {
     },
 }
 
-default_zone = node.metadata.get('firewalld', {}).get('default_zone')
 if node.metadata.get('firewalld', {}).get('default_zone'):
+    default_zone = node.metadata.get('firewalld', {}).get('default_zone')
     for interface in node.metadata['interfaces']:
         actions['firewalld_set_default_zone_{}'.format(interface)] = {
             'command': "firewall-cmd --permanent --zone={} --add-interface={}".format(default_zone, interface),
@@ -48,14 +48,41 @@ elif node.metadata.get('firewalld', {}).get('custom_zones', False):
         }
 
 for port in node.metadata.get('firewalld', {}).get('ports', {}):
-    actions['firewalld_add_port_{}'.format(port)] = {
-        'command': "firewall-cmd --permanent --add-port={}".format(port),
-        'unless': "firewall-cmd --list-ports | grep {}".format(port),
-        'cascade_skip': False,
-        'needs': [
-            "pkg_yum:firewalld",
-        ],
-        'triggers': [
-            "action:firewalld_reload",
-        ],
-    }
+    if node.metadata.get('firewalld', {}).get('default_zone'):
+        actions['firewalld_add_port_to_default_zone_{}'.format(port)] = {
+            'command': "firewall-cmd --permanent --zone={} --add-port={}".format(default_zone, port),
+            'unless': "firewall-cmd --zone={} --list-ports | grep {}".format(default_zone, port),
+            'cascade_skip': False,
+            'needs': [
+                "pkg_yum:firewalld",
+            ],
+            'triggers': [
+                "action:firewalld_reload",
+            ],
+        }
+    elif node.metadata.get('firewalld', {}).get('custom_zones', False):
+        for interface in node.metadata['interfaces']:
+            custom_zone = node.metadata.get('interfaces', {}).get(interface).get('firewalld_zone')
+            actions['firewalld_add_port_{}_to_custom_zone_{}'.format(port, custom_zone)] = {
+                'command': "firewall-cmd --permanent --zone={} --add-port={}".format(custom_zone, port),
+                'unless': "firewall-cmd --zone={} --list-ports | grep {}".format(custom_zone, port),
+                'cascade_skip': False,
+                'needs': [
+                    "pkg_yum:firewalld",
+                ],
+                'triggers': [
+                    "action:firewalld_reload",
+                ],
+            }
+    else:
+        actions['firewalld_add_port_{}'.format(port)] = {
+            'command': "firewall-cmd --permanent --add-port={}".format(port),
+            'unless': "firewall-cmd --list-ports | grep {}".format(port),
+            'cascade_skip': False,
+            'needs': [
+                "pkg_yum:firewalld",
+            ],
+            'triggers': [
+                "action:firewalld_reload",
+            ],
+        }
